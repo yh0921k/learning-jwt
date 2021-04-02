274fc8180850f4ec447fa5cdd34cefd3086f19f1
Yonghwi Kim <yh0921k@gmail.com>
2021-04-02 11:06:42 +0900

    [init] JWT 학습을 위한 프로젝트 초기화 및 기본 설정
    Dependencies
    - build.gradle 확인
    
    Git 추적 제외
    - .gitignore 확인

---
+++ src/main/java/study/learningjwt/LearningjwtApplication.java
@@ -0,0 +1,13 @@
+package study.learningjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningjwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningjwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/study/learningjwt/LearningjwtApplicationTests.java
@@ -0,0 +1,13 @@
+package study.learningjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningjwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
